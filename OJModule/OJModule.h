a32be98d426cb5ffab20b4ebe7963e4d4ea8c677
Oliver <oliver.johnsson@me.com>
2019-01-18 10:31:06 +0100

    Initial Commit

---
+++ OJModule/OJModule.h
@@ -0,0 +1,19 @@
+//
+//  OJModule.h
+//  OJModule
+//
+//  Created by Oliver Johnsson on 2019-01-18.
+//  Copyright © 2019 Oliver Johnsson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OJModule.
+FOUNDATION_EXPORT double OJModuleVersionNumber;
+
+//! Project version string for OJModule.
+FOUNDATION_EXPORT const unsigned char OJModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OJModule/PublicHeader.h>
+
+
